a13c15da358974bf7cdeb13291de636cb34ff503
d3monw1ngs <tezzielumsit87@gmail.com>
2023-10-19 11:26:55 +0800

    update styles and html

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,8 @@
 (() => {
     const refs = {
-      openModalBtn: document.querySelector(".data-modal-open"),
-      closeModalBtn: document.querySelector(".data-modal-close"),
-      modal: document.querySelector(".data-modal"),
+      openModalBtn: document.querySelector("[data-modal-open]"),
+      closeModalBtn: document.querySelector("[data-modal-close]"),
+      modal: document.querySelector("[data-modal]"),
     };
   
     refs.openModalBtn.addEventListener("click", toggleModal);
@@ -10,5 +10,6 @@
   
     function toggleModal() {
       refs.modal.classList.toggle("is-hidden");
+      refs.body.classList.toggle("no-scroll");
     }
   })();
